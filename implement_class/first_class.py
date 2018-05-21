62c8614f2fe13260fa3a810f889a46b90b3a35b7
sabih <siddiquisabih94@gmail.com>
2018-05-22 02:05:01 +0500

    implement first class

---
+++ implement_class/first_class.py
@@ -0,0 +1,16 @@
+class FirstClass():
+
+    def __init__(self , name):
+        self.name = name
+
+
+    def printMyName(self):
+        print(self.name.title())
+
+
+
+
+
+myClass = FirstClass('sabih siddiqui')
+
+myClass.printMyName()
