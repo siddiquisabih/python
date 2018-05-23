89ee86420ccf0cc935cd6f4163dfddb47839b1f7
sabih <sabih@texpo.com>
2018-05-23 11:33:48 +0500

    list like array

---
+++ List/list.py
@@ -0,0 +1,21 @@
+lists = [1, 2, 3, 4, 5, 6, 4]
+for a in lists:
+    print(a)
+
+
+number = list (range (2, 11, 2))
+print(number)
+
+
+newnumber = list(range(1,27))
+print(sum(newnumber))
+
+squares = [value-1 for value in range(1,11)]
+print(squares)
+for a in range (1,21):
+    print(a)
+
+
+new=['sabih' , 'mohiuddin', 'siddiqui']
+
+print(new[:])
